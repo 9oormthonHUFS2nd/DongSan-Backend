6b232d5576c678318083303ae577853ca4f79ddc
jorippppong <duelee75@gmail.com>
2024-07-24 16:50:59 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# DongSan-Backend
---
+++ src/main/java/com/dongsan/DongSanApplication.java
@@ -0,0 +1,13 @@
+package com.dongsan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongSanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongSanApplication.class, args);
+    }
+
+}
