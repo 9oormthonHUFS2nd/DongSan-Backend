97cb0f95921c5c6ca47122aafda516214a01780a
rjswjddn <rjswjddn@gmail.com>
2024-11-28 22:36:35 +0900

    fix: 유저 아이디 하드 코딩 수정

--- dongsan-api/src/main/java/com/dongsan/domains/user/controller/UserProfileController.java
+++ dongsan-api/src/main/java/com/dongsan/domains/user/controller/UserProfileController.java
@@ -2,6 +2,7 @@ package com.dongsan.domains.user.controller;
 
 import com.dongsan.apiResponse.ResponseFactory;
 import com.dongsan.apiResponse.SuccessResponse;
+import com.dongsan.domains.auth.security.oauth2.dto.CustomOAuth2User;
 import com.dongsan.domains.user.dto.response.GetBookmarksResponse;
 import com.dongsan.domains.user.dto.response.GetProfileResponse;
 import com.dongsan.domains.user.usecase.UserProfileUseCase;
@@ -9,6 +10,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,8 +27,10 @@ public class UserProfileController {
      */
     @Operation(summary = "사용자 프로필 조회")
     @GetMapping("/users/profile")
-    public ResponseEntity<SuccessResponse<GetProfileResponse>> getProfile() {
-        return ResponseFactory.ok(userProfileUsecase.getUserProfile(1L));
+    public ResponseEntity<SuccessResponse<GetProfileResponse>> getProfile(
+            @AuthenticationPrincipal CustomOAuth2User customOAuth2User
+    ) {
+        return ResponseFactory.ok(userProfileUsecase.getUserProfile(customOAuth2User.getMemberId()));
     }
 
     /**
@@ -36,8 +40,9 @@ public class UserProfileController {
     @GetMapping("/users/bookmarks/title")
     public ResponseEntity<SuccessResponse<GetBookmarksResponse>> getBookmarks(
             @RequestParam(required = false) Long bookmarkId,
-            @RequestParam(defaultValue = "10") Integer limit
+            @RequestParam(defaultValue = "10") Integer limit,
+            @AuthenticationPrincipal CustomOAuth2User customOAuth2User
     ) {
-        return ResponseFactory.ok(userProfileUsecase.getUserBookmarks(1L, bookmarkId, limit));
+        return ResponseFactory.ok(userProfileUsecase.getUserBookmarks(customOAuth2User.getMemberId(), bookmarkId, limit));
     }
 }
--- dongsan-api/src/main/java/com/dongsan/domains/walkway/controller/WalkwayController.java
+++ dongsan-api/src/main/java/com/dongsan/domains/walkway/controller/WalkwayController.java
@@ -2,6 +2,7 @@ package com.dongsan.domains.walkway.controller;
 
 import com.dongsan.apiResponse.ResponseFactory;
 import com.dongsan.apiResponse.SuccessResponse;
+import com.dongsan.domains.auth.security.oauth2.dto.CustomOAuth2User;
 import com.dongsan.domains.walkway.dto.request.CreateWalkwayRequest;
 import com.dongsan.domains.walkway.dto.response.CreateWalkwayResponse;
 import com.dongsan.domains.walkway.dto.response.GetWalkwaySearchResponse;
@@ -11,6 +12,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,16 +33,18 @@ public class WalkwayController {
     @Operation(summary = "산책로 등록")
     @PostMapping("")
     public ResponseEntity<SuccessResponse<CreateWalkwayResponse>> createWalkway(
-            @Validated @RequestBody CreateWalkwayRequest createWalkwayRequest
+            @Validated @RequestBody CreateWalkwayRequest createWalkwayRequest,
+            @AuthenticationPrincipal CustomOAuth2User customOAuth2User
     ) {
-        return ResponseFactory.created(walkwayUseCase.createWalkway(createWalkwayRequest, 1L));
+        return ResponseFactory.created(walkwayUseCase.createWalkway(createWalkwayRequest, customOAuth2User.getMemberId()));
     }
 
     @GetMapping("/{walkwayId}")
     public ResponseEntity<SuccessResponse<GetWalkwayWithLikedResponse>> getWalkway(
-            @PathVariable Long walkwayId
+            @PathVariable Long walkwayId,
+            @AuthenticationPrincipal CustomOAuth2User customOAuth2User
     ) {
-        return ResponseFactory.ok(walkwayUseCase.getWalkwayWithLiked(walkwayId, 1L));
+        return ResponseFactory.ok(walkwayUseCase.getWalkwayWithLiked(walkwayId, customOAuth2User.getMemberId()));
     }
 
     @GetMapping("")
@@ -53,8 +57,9 @@ public class WalkwayController {
             @RequestParam(defaultValue = "0") Long lastId,
             @RequestParam(defaultValue = "0") Double rating,
             @RequestParam(defaultValue = "0") Integer likes,
-            @RequestParam(defaultValue = "10") Integer size
+            @RequestParam(defaultValue = "10") Integer size,
+            @AuthenticationPrincipal CustomOAuth2User customOAuth2User
     ) {
-        return ResponseFactory.ok(walkwayUseCase.getWalkwaysSearch(1L, type, latitude, longitude, distance, hashtags, lastId, rating, likes, size));
+        return ResponseFactory.ok(walkwayUseCase.getWalkwaysSearch(customOAuth2User.getMemberId(), type, latitude, longitude, distance, hashtags, lastId, rating, likes, size));
     }
 }
