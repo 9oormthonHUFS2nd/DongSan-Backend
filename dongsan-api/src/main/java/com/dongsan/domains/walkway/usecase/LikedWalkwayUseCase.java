f9de03da85bce2faa0076a64a4e7aa1cf940326f
rjswjddn <rjswjddn@gmail.com>
2024-12-03 21:10:56 +0900

    feature(DB-67): 산책로 좋아요 API

---
+++ dongsan-api/src/main/java/com/dongsan/domains/walkway/usecase/LikedWalkwayUseCase.java
@@ -0,0 +1,38 @@
+package com.dongsan.domains.walkway.usecase;
+
+import com.dongsan.common.annotation.UseCase;
+import com.dongsan.domains.member.entity.Member;
+import com.dongsan.domains.member.service.MemberQueryService;
+import com.dongsan.domains.walkway.entity.LikedWalkway;
+import com.dongsan.domains.walkway.entity.Walkway;
+import com.dongsan.domains.walkway.mapper.LikedWalkwayMapper;
+import com.dongsan.domains.walkway.service.LikedWalkwayCommandService;
+import com.dongsan.domains.walkway.service.WalkwayQueryService;
+import com.dongsan.error.code.MemberErrorCode;
+import com.dongsan.error.code.WalkwayErrorCode;
+import com.dongsan.error.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+
+@UseCase
+@RequiredArgsConstructor
+public class LikedWalkwayUseCase {
+    private final LikedWalkwayCommandService likedWalkwayCommandService;
+    private final MemberQueryService memberQueryService;
+    private final WalkwayQueryService walkwayQueryService;
+
+    public void createLikedWalkway(Long memberId, Long walkwayId) {
+        Member member = memberQueryService.readMember(memberId)
+                .orElseThrow(() -> new CustomException(MemberErrorCode.MEMBER_NOT_FOUND));
+
+        Walkway walkway = walkwayQueryService.getWalkway(walkwayId)
+                .orElseThrow(() -> new CustomException(WalkwayErrorCode.WALKWAY_NOT_FOUND));
+
+        if (likedWalkwayCommandService.existsLikedWalkwayByMemberAndWalkway(member, walkway)) {
+            return;
+        }
+
+        LikedWalkway likedWalkway = LikedWalkwayMapper.toLikedWalkway(member, walkway);
+
+        likedWalkwayCommandService.createLikedWalkway(likedWalkway);
+    }
+}
--- dongsan-domain/src/main/java/com/dongsan/domains/walkway/repository/LikedWalkwayRepository.java
+++ dongsan-domain/src/main/java/com/dongsan/domains/walkway/repository/LikedWalkwayRepository.java
@@ -1,6 +1,8 @@
 package com.dongsan.domains.walkway.repository;
 
+import com.dongsan.domains.member.entity.Member;
 import com.dongsan.domains.walkway.entity.LikedWalkway;
+import com.dongsan.domains.walkway.entity.Walkway;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface LikedWalkwayRepository extends JpaRepository<LikedWalkway, Long> {
     Optional<LikedWalkway> findByMemberIdAndWalkwayId(Long memberId, Long walkwayId);
+
+    Boolean existsByMemberAndWalkway(Member member, Walkway walkway);
 }
