98a81600d28e7792fe00b8b2a699268b14626e7e
rjswjddn <rjswjddn@gmail.com>
2025-01-31 18:58:10 +0900

    refactor: 리뷰 별점 최대, 최소값

--- core-domain/src/main/java/com/dongsan/domains/walkway/dto/request/CreateReviewRequest.java
+++ core-domain/src/main/java/com/dongsan/domains/walkway/dto/request/CreateReviewRequest.java
@@ -1,11 +1,15 @@
 package com.dongsan.domains.walkway.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
 public record CreateReviewRequest(
         @NotNull(message = "별점을 입력해주세요.")
+        @Min(1)
+        @Max(5)
         Integer rating,
         @NotBlank(message = "리뷰 내용을 입력해주세요.")
         @Length(min = 1, max = 100)
