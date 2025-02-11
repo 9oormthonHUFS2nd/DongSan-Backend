30e8e9186217fed29ce32c2765bce9a359be8b23
rjswjddn <rjswjddn@gmail.com>
2025-02-12 04:08:32 +0900

    test(DB-163): 리뷰 가능한 모든 산책 기록 Test

--- app-external-api/src/test/java/com/dongsan/domains/user/controller/UserWalkwayControllerTest.java
+++ app-external-api/src/test/java/com/dongsan/domains/user/controller/UserWalkwayControllerTest.java
@@ -14,7 +14,11 @@ import com.dongsan.domains.member.entity.Member;
 import com.dongsan.domains.user.response.WalkwayListResponse;
 import com.dongsan.domains.user.usecase.UserWalkwayUseCase;
 import com.dongsan.domains.walkway.entity.Walkway;
+import com.dongsan.domains.walkway.entity.WalkwayHistory;
 import com.dongsan.domains.walkway.service.WalkwayQueryService;
+import com.dongsan.domains.walkway.usecase.WalkwayHistoryUseCase;
+import fixture.WalkwayFixture;
+import fixture.WalkwayHistoryFixture;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -26,11 +30,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 @WebMvcTest(UserWalkwayController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -43,6 +49,9 @@ class UserWalkwayControllerTest {
     UserWalkwayUseCase userWalkwayUseCase;
     @MockBean
     WalkwayQueryService walkwayQueryService;
+    @MockBean
+    WalkwayHistoryUseCase walkwayHistoryUseCase;
+
     final Member member = createMemberWithId(1L);
     final CustomOAuth2User customOAuth2User = new CustomOAuth2User(member);
 
@@ -112,5 +121,37 @@ class UserWalkwayControllerTest {
                     .andReturn();
         }
 
+    }
+
+    @Nested
+    @DisplayName("getUserWalkwayHistory 메서드는")
+    class Describe_getUserWalkwayHistory {
+
+        @Test
+        @DisplayName("리뷰 가능한 산책로 이용 기록을 반환한다.")
+        void it_returns_walkway_histories() throws Exception {
+            // Given
+            int size = 10;
+            Long lastId = 1L;
+            Long memberId = member.getId();
+            Walkway walkway = WalkwayFixture.createWalkwayWithId(1L, null);
+
+            List<WalkwayHistory> histories = List.of(
+                    WalkwayHistoryFixture.createWalkwayHistoryWithId(1L, null, walkway),
+                    WalkwayHistoryFixture.createWalkwayHistoryWithId(2L, null, walkway)
+            );
+
+            when(walkwayHistoryUseCase.getUserWalkwayHistories(memberId, lastId, size)).thenReturn(histories);
+
+            // When
+            ResultActions response = mockMvc.perform(get("/users/walkways/history")
+                            .param("size", String.valueOf(size))
+                            .param("lastId", String.valueOf(lastId))
+                    .contentType(MediaType.APPLICATION_JSON));
+
+            // Then
+            response.andExpect(status().isOk())
+                    .andExpect(jsonPath("$.data.walkwayHistories.size()").value(2));
+        }
     }
 }
